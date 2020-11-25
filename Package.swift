9b1658632a423db5d7b14e1431eb1d6ad5f33295
Alin Petrus <alin.petrus@softvision.com>
2020-11-25 16:45:54 +0200

    Fixed naming issue

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SaveTheWorldBinary", url: "https://github.com/petrusalin/SaveTheWorldBinary.git", checksum: "c979916abeb9c6a7cad9c175b41bafe4b06f5b5f432d036c75e51de00e57a41b")
+        .binaryTarget(name: "SaveTheWorldBinary", url: "SaveTheWorldFramework.xcframework.zip", checksum: "c979916abeb9c6a7cad9c175b41bafe4b06f5b5f432d036c75e51de00e57a41b")
     ]
 )
