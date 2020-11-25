f689808624605f72a0982c25bc621bcf90f76472
Alin Petrus <alin.petrus@softvision.com>
2020-11-25 16:47:59 +0200

    Naming one more time

--- Package.swift
+++ Package.swift
@@ -16,6 +16,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SaveTheWorldBinary", url: "SaveTheWorldFramework.xcframework.zip", checksum: "c979916abeb9c6a7cad9c175b41bafe4b06f5b5f432d036c75e51de00e57a41b")
+        .binaryTarget(name: "SaveTheWorldBinary",
+                      url: "https://github.com/petrusalin/SaveTheWorldBinary/blob/main/SaveTheWorldFramework.xcframework.zip",
+                      checksum: "c979916abeb9c6a7cad9c175b41bafe4b06f5b5f432d036c75e51de00e57a41b")
     ]
 )
