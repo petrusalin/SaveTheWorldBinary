5ff90ced6c0510b704c440faa75ca41b5ef696b7
Alin Petrus <alin.petrus@softvision.com>
2020-11-26 16:37:09 +0200

    Using localhost for test

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "SaveTheWorldBinary",
-                      url: "https://github.com/petrusalin/resources/blob/master/SaveTheWorldFramework.xcframework.zip",
+                      url: "http://localhost/~alin.petrus/SaveTheWorldFramework.xcframework.zip",
                       checksum: "c979916abeb9c6a7cad9c175b41bafe4b06f5b5f432d036c75e51de00e57a41b")
     ]
 )
